1429d6d033cc09a94e3c81f46b2484b7d76b1706
Rey Joshua <reyjoshuamacarat@gmail.com>
2020-09-09 20:45:09 +0800

    recreated project

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7pk@*ijb5o+ma31au#l5-&w-vxuq&j!-7nu-9$08iu%f+(ckf'
+SECRET_KEY = '6wpt_@a4x93s96ba70aiyhom0q9^6)ju%^#)^snr#kjusz))gd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,10 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #project apps
-    'core.product'
-
-    #django pre-built apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
