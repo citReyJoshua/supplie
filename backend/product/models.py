d9da8bfe40ec186790d7b827b61be0f24b46a0a0
Rey Joshua <reyjoshuamacarat@gmail.com>
2020-09-15 19:49:37 +0800

    created product, product-image models

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from backend import globals
 
-# Create your models here.
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='frontend/static/images/product/product-images', verbose_name='Product Image')
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Product Name', max_length=globals.MAX_LENGTH_DEFAULT)
+    category = models.CharField(max_length=globals.MAX_LENGTH_SHORT)
+    brand = models.CharField(max_length=globals.MAX_LENGTH_SHORT, blank=True, null=True)
+    color = models.CharField(max_length=globals.MAX_LENGTH_SHORT, blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    no_of_stocks = models.PositiveIntegerField()
+    image = models.ForeignKey(ProductImage, on_delete=models.CASCADE, verbose_name='Image')
